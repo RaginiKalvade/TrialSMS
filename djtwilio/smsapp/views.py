278328350c534588e600c56d749dfc8547aa3769
Mikkilineni Aarthi <aarthimikki@gmail.com>
2020-08-16 15:09:38 +0530

    chaged views.py

--- djtwilio/smsapp/views.py
+++ djtwilio/smsapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from twilio.rest import Client
+from django.conf import settings
 # Create your views here.  
+
+to = '+917470400587'
+client = Client(settings.TWILIO_ACCOUNT_SID,settings.TWILIO_AUTH_TOKEN)
+response = client.messages.create(body='Hello testing twilio',to=to,from_=settings.TWILIO_PHONE_NUMBER)
